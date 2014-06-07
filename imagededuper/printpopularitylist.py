07545b119d576efd1cdbcabf915d1309ee2bea29
Chris Speck <cgspeck@gmail.com>
2014-06-07 14:00:26 +1000

    Getinfo to return dictionary structures that match deduper

--- imagededuper/printpopularitylist.py
+++ imagededuper/printpopularitylist.py
@@ -16,7 +16,8 @@ def get_data(session):
     for filehash, count in session.query(ImageFile.filehash, func.count('*')
             .label('hash_count')).group_by(ImageFile.filehash)\
             .having(func.count('*') > 1).order_by('hash_count desc'):
-        qry = session.query(ImageFile.name,
+        qry = session.query(ImageFile.id, ImageFile.name,
+            ImageFile.fullpath,
             func.char_length(ImageFile.name).label('namelen'))\
             .filter(ImageFile.filehash == filehash)
         assert qry.count() == count
@@ -27,12 +28,16 @@ def get_data(session):
         keep_suggestion = None
 
         for result in qry:
-            files.append(result.name)
+            files.append(dict(name=result.name, fullpath=result.fullpath,
+                id=result.id))
             if result.namelen > max_len:
-                keep_suggestion = result.name
+                keep_suggestion = result
                 max_len = result.namelen
         # make sure we have set a file to save
         assert keep_suggestion
+        keep_suggestion = dict(name=keep_suggestion.name,
+            fullpath=keep_suggestion.fullpath, id=keep_suggestion.id)
+
         results.append(dict(hash=filehash, count=count, files=files,
             keep_suggestion=keep_suggestion))
 
@@ -46,7 +51,7 @@ def PrintPopularityList(session, mode):
         for result in results:
             # print row by row COUNT, SUGGESTION_TO_KEEP
             print("{count}, {name}".format(count=result['count'],
-                name=result['keep_suggestion']))
+                name=result['keep_suggestion']['name']))
     else:
             # pretty print the json objects
             pprint.pprint(results)
